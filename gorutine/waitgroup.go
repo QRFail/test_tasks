18d5527621db80e404eba6c447baac79b0f1d942
Фаиль <fail.gataullin@amopoint.ru>
2022-10-19 14:41:45 +0500

    mutex

--- gorutine/waitgroup.go
+++ gorutine/waitgroup.go
@@ -1,9 +1,19 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
 
+	//writeWaitGroup()
+	writeWithMutex()
+
+}
+
+func writeWaitGroup() {
 	var wg sync.WaitGroup
 
 	for i := 0; i < 10; i++ {
@@ -17,4 +27,33 @@ func main() {
 	wg.Wait()
 
 	println("Exit")
+}
+
+func writeWithMutex() {
+	var wg sync.WaitGroup
+	var mx sync.Mutex
+
+	count := 0
+	start := time.Now()
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(s int) {
+			defer wg.Done()
+
+			time.Sleep(time.Nanosecond)
+
+			mx.Lock()
+			count++
+			mx.Unlock()
+
+		}(i)
+	}
+
+	wg.Wait()
+
+	delta_time := time.Now().Sub(start).Seconds()
+
+	fmt.Printf("Count %d\n", count)
+	fmt.Println(delta_time)
 }
